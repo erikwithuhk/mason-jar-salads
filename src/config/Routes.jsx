8a95d39b773ebfa40d4cc239cc360e64d3989380
Erik Jönsson <efjonsson@gmail.com>
2016-09-21 23:22:53 -0400

    route to login when not logged in

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
-// import requireAuth from '../utils/requireAuth.js';
+import requireAuth from '../utils/requireAuth.js';
 import App from '../components/App.jsx';
 import Register from '../components/Register.jsx';
 import Login from '../components/Login.jsx';
@@ -8,7 +8,7 @@ import Login from '../components/Login.jsx';
 const routes = () => (
   <Router history={hashHistory} >
     <Route path="/" >
-      <IndexRoute component={App} />
+      <IndexRoute component={App} onEnter={requireAuth} />
       <Route path="register" component={Register} />
       <Route path="login" component={Login} />
     </Route>
