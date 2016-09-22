af236d4f56e2c12043366b060ce5d90442227231
Erik Jönsson <efjonsson@gmail.com>
2016-09-22 00:36:31 -0400

    set login state based on input values

--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const stateObject = {};
+    const stateKey = e.target.name;
+    stateObject[stateKey] = e.target.value;
+    this.setState(stateObject);
+  }
   render() {
     return (
       <div className="welcome container">
@@ -10,13 +24,17 @@ class Login extends Component {
           <h3 className="authentication-form__header">Log in</h3>
           <input
             className="authentication-form__email-input"
+            name="email"
             type="email"
             placeholder="Email"
+            onChange={this.handleChange}
           />
           <input
             className="authentication-form__password-input"
+            name="password"
             type="password"
             placeholder="Password"
+            onChange={this.handleChange}
           />
           <input
             className="authentication-form__submit btn btn--primary"
