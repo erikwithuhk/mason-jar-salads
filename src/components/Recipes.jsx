979a9d2cc8aee7469a1efd7709b39563413d8937
Erik Jönsson <efjonsson@gmail.com>
2016-09-22 13:07:01 -0400

    change my-recipe to recipe

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -3,20 +3,20 @@ import React, { Component } from 'react';
 class Recipes extends Component {
   render() {
     return (
-      <section className="my-recipes clearfix">
-        <article className="my-recipes__recipe-card">
+      <section className="recipes clearfix">
+        <article className="recipes__recipe-card">
           <a href="#">
-            <div className="my-recipes__recipe-card__image" style={{ backgroundImage: 'url(\'https://media1.popsugar-assets.com/files/thumbor/JGKtKFMeJL66e9S2fjQpLNR5X1w/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/12/12/730/n/1922729/c5008f3d8961ebbb_10843733_894977147188435_1798758311_n/i/5-Ingredient-Salad.jpg\')' }} />
-            <div className="my-recipes__recipe-card__name-and-user">
+            <div className="recipes__recipe-card__image" style={{ backgroundImage: 'url(\'https://media1.popsugar-assets.com/files/thumbor/JGKtKFMeJL66e9S2fjQpLNR5X1w/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/12/12/730/n/1922729/c5008f3d8961ebbb_10843733_894977147188435_1798758311_n/i/5-Ingredient-Salad.jpg\')' }} />
+            <div className="recipes__recipe-card__name-and-user">
               <h4 className="recipe-title">Chicken and mozzarella salad</h4>
               <p className="username">erikwithuhk</p>
             </div>
           </a>
         </article>
-        <article className="my-recipes__recipe-card">
+        <article className="recipes__recipe-card">
           <a href="#">
-            <div className="my-recipes__recipe-card__image" style={{ backgroundImage: 'url(\'https://media1.popsugar-assets.com/files/thumbor/JGKtKFMeJL66e9S2fjQpLNR5X1w/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/12/12/730/n/1922729/c5008f3d8961ebbb_10843733_894977147188435_1798758311_n/i/5-Ingredient-Salad.jpg\')' }} />
-            <div className="my-recipes__recipe-card__name-and-user">
+            <div className="recipes__recipe-card__image" style={{ backgroundImage: 'url(\'https://media1.popsugar-assets.com/files/thumbor/JGKtKFMeJL66e9S2fjQpLNR5X1w/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/12/12/730/n/1922729/c5008f3d8961ebbb_10843733_894977147188435_1798758311_n/i/5-Ingredient-Salad.jpg\')' }} />
+            <div className="recipes__recipe-card__name-and-user">
               <h4 className="recipe-title">Chicken and mozzarella salad</h4>
               <p className="username">erikwithuhk</p>
             </div>
