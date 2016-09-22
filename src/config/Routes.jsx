9e7540b8fc6e7b91ba26b6f795f5ece39f382aa3
Erik Jönsson <efjonsson@gmail.com>
2016-09-21 23:06:15 -0400

    set up register and login as react components

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import requireAuth from '../utils/auth.js';
 import App from '../components/App.jsx';
 import Register from '../components/Register.jsx';
 import Login from '../components/Login.jsx';
@@ -7,7 +8,7 @@ import Login from '../components/Login.jsx';
 const Routes = () => (
   <Router history={hashHistory} >
     <Route path="/">
-      <IndexRoute component={App} />
+      <IndexRoute component={App} onEnter={requireAuth} />
       <Route path="register" component={Register} />
       <Route path="login" component={Login} />
     </Route>
