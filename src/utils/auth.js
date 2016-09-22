4d2c9b974d79e928e74aa8a9fe9e3dd0c12298ca
Erik Jönsson <efjonsson@gmail.com>
2016-09-21 23:00:52 -0400

    create register and login component files, complete requireAuth

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,10 @@ import firebase from '../../firebase.config.js';
 
 function requireAuth(nextState, replace) {
   if (firebase.auth().currentUser === null) {
-
+    replace({
+      pathname: '/login',
+      state: { nextPathname: nextState.location.pathname },
+    });
   }
 }
 
