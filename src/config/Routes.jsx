1bf69b4620bb2264d5c89c3646e3190ad1c383a3
Erik Jönsson <efjonsson@gmail.com>
2016-09-21 22:58:37 -0400

    set register and login routes

--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from '../components/App.jsx';
+import Register from '../components/Register.jsx';
+import Login from '../components/Login.jsx';
 
 const Routes = () => (
   <Router history={hashHistory} >
     <Route path="/">
       <IndexRoute component={App} />
+      <Route path="register" component={Register} />
+      <Route path="login" component={Login} />
     </Route>
   </Router>
 );
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,2 +1,10 @@
 import React from 'react';
-// import firebase from 'firebase';
+import firebase from '../../firebase.config.js';
+
+function requireAuth(nextState, replace) {
+  if (firebase.auth().currentUser === null) {
+
+  }
+}
+
+module.exports = requireAuth;
