3bcbd9b1c9da79f9289bf799299e1762091f9490
Erik Jönsson <efjonsson@gmail.com>
2016-09-21 23:33:11 -0400

    copy over html for authentication page

--- src/components/authentication/Authentication.jsx
+++ src/components/authentication/Authentication.jsx
@@ -4,7 +4,38 @@ import { withRouter } from 'react-router';
 class Authentication extends React.Component {
   render() {
     return (
-      <div>Authentication</div>
+      <div className="welcome container">
+        <h1 className="welcome__page-header">Mason Jar Salads</h1>
+        <form className="authentication-form">
+          <h3 className="authentication-form__header">Create an account</h3>
+          <input
+            className="authentication-form__email-input"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="authentication-form__password-input"
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            className="authentication-form__username-input"
+            type="username"
+            placeholder="Username"
+          />
+          <input
+            className="authentication-form__submit"
+            type="submit"
+            value="Create account"
+          />
+          <p
+            className="welcome__authentication-toggle-text"
+          >
+            Already have an account? Log in here.
+          </p>
+        </form>
+      </div>
+
     );
   }
 }
