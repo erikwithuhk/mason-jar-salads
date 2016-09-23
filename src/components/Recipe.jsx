a1f1b84e13d2195de1250e82c18a016fe1f5e26e
Erik Jönsson <efjonsson@gmail.com>
2016-09-23 13:59:05 -0400

    style section padding

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 class Recipe extends Component {
   render() {
     return (
-      <h1 className="recipe-view__name">Recipe name</h1>
-      <h4 className="recipe-view__author">Created by: <a href="#">erikwithuhk</a></h4>
+      <section className="recipe-view">
+        <h1 className="recipe-view__name">Recipe name</h1>
+        <h4 className="recipe-view__author">Created by: <a href="#">erikwithuhk</a></h4>
 
-      <h3 className="ingredients-header">Ingredients</h3>
-      <ul className="ingredients-list">
-        <li className="ingredients-list__ingredient">1 cup kale</li>
-        <li className="ingredients-list__ingredient">2 tbsp dressing</li>
-      </ul>
+        <h3 className="ingredients-header">Ingredients</h3>
+        <ul className="ingredients-list">
+          <li className="ingredients-list__ingredient">1 cup kale</li>
+          <li className="ingredients-list__ingredient">2 tbsp dressing</li>
+        </ul>
+      </section>
     );
   }
 }
