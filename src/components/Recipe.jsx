55f0e65bf159f49faa129bd646b2069515a2f204
Erik Jönsson <efjonsson@gmail.com>
2016-09-23 13:46:15 -0400

    create ingredient list jsx

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 class Recipe extends Component {
   render() {
     return (
-      <h1>Recipe name</h1>
-      <h4>Recipe author</h4>
+      <h1 className="recipe-view__name">Recipe name</h1>
+      <h4 className="recipe-view__author">Created by: <a href="#">erikwithuhk</a></h4>
+
+      <h3 className="ingredients-header">Ingredients</h3>
+      <ul className="ingredients-list">
+        <li className="ingredients-list__ingredient">1 cup kale</li>
+        <li className="ingredients-list__ingredient">2 tbsp dressing</li>
+      </ul>
     );
   }
 }
