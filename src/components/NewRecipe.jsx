45d4dd040dcfc36ec6c37132c73666161bf74dba
Erik Jönsson <efjonsson@gmail.com>
2016-09-23 11:49:41 -0400

    create new recipe component and link route

---
+++ src/components/NewRecipe.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewRecipe extends Component {
+  render() {
+    return (
+      <form>
+        <input type="file" />
+      </form>
+    );
+  }
+}
+
+export default NewRecipe;
--- src/config/Routes.jsx
+++ src/config/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import requireAuth from '../utils/requireAuth.js';
 import App from '../components/App.jsx';
+import NewRecipe from '../components/NewRecipe.jsx';
 import Recipes from '../components/Recipes.jsx';
 import Authentication from '../components/authentication/Authentication.jsx';
 import Registration from '../components/authentication/Registration.jsx';
@@ -11,6 +12,9 @@ const routes = () => (
   <Router history={hashHistory} >
     <Route path="/" component={App} >
       <IndexRoute component={Recipes} onEnter={requireAuth} />
+      <Route path="recipes">
+        <Route path="new" component={NewRecipe} />
+      </Route>
       <Route path="welcome" component={Authentication} />
       <Route path="register" component={Registration} />
       <Route path="login" component={Login} />
