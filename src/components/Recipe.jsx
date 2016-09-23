5cdc7bce8fdf07aa5cdab83fae68fd55b8f7726b
Erik Jönsson <efjonsson@gmail.com>
2016-09-23 13:42:26 -0400

    create component

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Recipe extends Component {
+  render() {
+    return (
+      <h1>Recipe name</h1>
+      <h4>Recipe author</h4>
+    );
+  }
+}
+
+export default Recipe;
