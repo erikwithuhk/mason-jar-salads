8942fb05464ecb1a54092c298bd2e900f1ded418
Erik Jönsson <efjonsson@gmail.com>
2016-09-25 22:00:17 -0400

    attach event listeners to inputs

--- src/components/NewRecipe.jsx
+++ src/components/NewRecipe.jsx
@@ -12,7 +12,7 @@ class NewRecipe extends Component {
         veggies: '',
         sweet: '',
         crunchy: '',
-      }
+      },
     };
   }
   handleChange(e) {
@@ -26,68 +26,75 @@ class NewRecipe extends Component {
     return (
       <section className="new-recipe">
         <form className="new-recipe-form" onSubmit={this.handleSubmit}>
-          <label>
+          <label htmlFor="recipe-name">
             <h3>Recipe name</h3>
             <input
               className="new-recipe-form__input"
               type="text"
               name="recipe-name"
               placeholder="My delicious salad"
+              onChange={this.handleChange}
             />
           </label>
           <h3>Ingredients</h3>
-          <label>
+          <label htmlFor="greens">
             <h4>Greens</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="greens"
               placeholder="Kale, romaine lettuce, spinach..."
+              onChange={this.handleChange}
             />
           </label>
-          <label>
+          <label htmlFor="beans">
             <h4>Beans</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="beans"
               placeholder="Chick peas, white beans, lentils..."
+              onChange={this.handleChange}
             />
           </label>
-          <label>
+          <label htmlFor="grains">
             <h4>Grains</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="grains"
               placeholder="Quinoa, bulgar wheat, pasta..."
+              onChange={this.handleChange}
             />
           </label>
-          <label>
+          <label htmlFor="veggies">
             <h4>Veggies</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="veggies"
               placeholder="Carrots, peppers, roasted squash..."
+              onChange={this.handleChange}
             />
           </label>
-          <label>
+          <label htmlFor="sweet">
             <h4>Sweet</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="sweet"
               placeholder="Raisins, diced apple, mango, ..."
+              onChange={this.handleChange}
             />
           </label>
-          <label>
+          <label htmlFor="crunchy">
             <h4>Crunchy</h4>
             <input
               className="new-recipe-form__input"
               type="text"
               name="crunchy"
               placeholder="Almonds, walnuts, croutons..."
+              onChange={this.handleChange}
             />
           </label>
           <input
