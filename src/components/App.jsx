6e1e5394af7ac2de13c4dd7f1f05ccd1c34e2fad
Erik Jönsson <efjonsson@gmail.com>
2016-09-26 11:07:36 -0400

    add all recipes and my recipes to nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -50,7 +50,8 @@ class App extends Component {
           <nav className="top-nav">
             <h1 className="top-nav__brand">Mason Jar Salads</h1>
             <nav className="top-nav__sub-nav">
-              <Link to="/" >My recipes</Link>
+              <Link to="/" >All recipes</Link>
+              <Link to={`/${this.state.username}`} >My recipes</Link>
               <Link to="/welcome" onClick={this.signOut}>Sign out</Link>
             </nav>
           </nav>
