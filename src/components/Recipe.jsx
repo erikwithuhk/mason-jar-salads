4aa82ac83e76232d5ac7aa602132f9d2256ec550
Erik Jönsson <efjonsson@gmail.com>
2016-09-25 22:57:03 -0400

    create update and delete buttons

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -16,16 +16,6 @@ class Recipe extends Component {
       },
     };
   }
-  componentWillReceiveProps(nextprops) {
-    const id = this.props.params.id;
-    nextprops.recipes.forEach((recipe) => {
-      if (recipe.id === id) {
-        this.setState({
-          currentRecipe: recipe,
-        });
-      }
-    });
-  }
   componentDidMount() {
     const id = this.props.params.id;
     this.props.recipes.forEach((recipe) => {
@@ -36,30 +26,59 @@ class Recipe extends Component {
       }
     });
   }
+  componentWillReceiveProps(nextprops) {
+    const id = this.props.params.id;
+    nextprops.recipes.forEach((recipe) => {
+      if (recipe.id === id) {
+        this.setState({
+          currentRecipe: recipe,
+        });
+      }
+    });
+  }
+  handleUpdate() {
+
+  }
+  handleDelete() {
+    
+  }
   render() {
     const recipe = this.state.currentRecipe;
-    // let keyValue = 0;
-    // const ingredientElements = recipe.ingredients.map((ingredient) => {
-    //   keyValue += 1;
-    //   return (
-    //     <li key={keyValue} className="ingredients-list__ingredient">{ingredient}</li>
-    //   );
-    // });
     return (
       <section className="recipe-view">
         <h1 className="recipe-view__name">{recipe.name}</h1>
-        <h4 className="recipe-view__author">Created by: <a href="#">{recipe.username}</a></h4>
+        <h4 className="recipe-view__author">Created by: {recipe.username}</h4>
         <p className="yield">Makes 4 1-quart mason jar salads</p>
         <h3 className="ingredients-header">Ingredients</h3>
         <ul className="ingredients-list">
           <li className="ingredients-list__ingredient">4 cups {recipe.ingredients.greens}</li>
-          <li className="ingredients-list__ingredient">4 cups {recipe.ingredients.grains}, cooked</li>
+          <li
+            className="ingredients-list__ingredient"
+          >
+            4 cups {recipe.ingredients.grains}, cooked
+          </li>
           <li className="ingredients-list__ingredient">1 15-oz can {recipe.ingredients.beans}</li>
           <li className="ingredients-list__ingredient">2 cups {recipe.ingredients.veggies}</li>
           <li className="ingredients-list__ingredient">2 cups {recipe.ingredients.sweet}</li>
           <li className="ingredients-list__ingredient">1 cup {recipe.ingredients.crunchy}</li>
-          <li className="ingredients-list__ingredient">1/2 cup {recipe.ingredients.dressing} dressing</li>
+          <li
+            className="ingredients-list__ingredient"
+          >
+            1/2 cup {recipe.ingredients.dressing} dressing
+          </li>
         </ul>
+        <button
+          className="recipe-button update-button"
+          onClick={this.handleUpdate}
+        >
+          Update recipe
+        </button>
+        <button
+          className="recipe-button delete-button"
+          onClick={this.handleDelete}
+        >
+          Delete recipe
+        </button>
       </section>
     );
   }
