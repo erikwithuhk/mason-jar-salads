a4070ed71d00c0e1593cadff623168dd8d92ae2c
Erik Jönsson <efjonsson@gmail.com>
2016-09-26 23:42:21 -0400

    change recipe card default image

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -5,7 +5,7 @@ const recipeCard = ({ id, recipeName, authorName }) => {
   return (
     <article className="recipes__recipe-card">
       <Link to={`/recipes/${id}`}>
-        <div className="recipes__recipe-card__image" style={{ backgroundImage: 'url(\'https://media1.popsugar-assets.com/files/thumbor/JGKtKFMeJL66e9S2fjQpLNR5X1w/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2014/12/12/730/n/1922729/c5008f3d8961ebbb_10843733_894977147188435_1798758311_n/i/5-Ingredient-Salad.jpg\')' }} />
+        <div className="recipes__recipe-card__image" />
         <div className="recipes__recipe-card__name-and-user">
           <h4 className="recipe-title">{recipeName}</h4>
           <p className="username">{authorName}</p>
